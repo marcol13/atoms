cb4b2ebb865ec6afe6de2da0441ca58c28abc4a0
Marcin <marcinkrueger@onet.eu>
2020-09-09 10:53:58 +0200

    Add atom and rectangle class

---
+++ script/script.js
@@ -0,0 +1,16 @@
+var n = 15 //ilość atomów
+var r = 5 //promień atomów
+
+class Atom{
+    m = 1 //masa
+    constructor(r,x,y,vx,vy){
+        this.r = r //promień
+        this.pos_vector = [x,y] //wektor położenia
+        this.vel_vector = [vx,vy] //wektor prędkości
+    }
+    
+}
+
+const Rect{
+    
+}
