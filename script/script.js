4792af57bf0d99b6107cfd6c65dd72d81cda122a
Marcin <marcinkrueger@onet.eu>
2020-09-09 11:03:36 +0200

    Add wall bouncing method

--- script/script.js
+++ script/script.js
@@ -9,8 +9,23 @@ class Atom{
         this.vel_vector = [vx,vy] //wektor prędkości
     }
     
+    function wall_bounce(opt){ //true - odbicie od górnej lub od dolnej ścianki, false - odbicie od prawej lub lewej ścianki
+        if(opt === true)
+            this.vel_vector[1] *= -1
+        else
+            this.vel_vector[0] *= -1
+    }
 }
 
-const Rect{
-    
+class Rect{
+    constructor(etah, etal){
+        this.etah = etah >= 20 ? etah : 20, //współczynnik h
+        this.etal = etal >= 20 ? etal : 20, //współczynnik l
+        this.h = this.etah * r, //wysokość
+        this.l = this.etal * r //szerokość
+    }
 }
+
+// function change_n(x){
+//     if x > 1/4*()
+// }
